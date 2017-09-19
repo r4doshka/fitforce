4c183fcd1606e0aa7df5ed23a705b5696e2ca066
jovellir@gmail.com <721680234b>
2017-09-19 17:17:23 +0600

    add responsive

--- app/modules/scriptName.js
+++ app/modules/scriptName.js
@@ -2,7 +2,18 @@ import $ from 'jquery';
 
 const scriptName = () => {
 
+    $('.fitforce-menu-toggle').on('click', function(){
+        $('.fitforce-menu-panel-mobile').slideToggle();
+        $('body').toggleClass('no-scroll')
+    });
 
+    $('.anchor').on('click', function(e){
+        e.preventDefault();
+        var elem = $(this).attr('href'),
+            positionscroll = $(elem).offset().top;
+        $('body,html').animate({scrollTop:positionscroll}, 1000);
+
+    });
 };
 
 export default scriptName();
